616652da6e88593779b007d5a4b286cee20e0875
SamuelRicardos <samuelric4rdo@gmail.com>
2025-05-09 04:33:27 -0300

    criacao de responsividade da tela de cadastro e de senha

--- src/pages/ResetPassword/index.tsx
+++ src/pages/ResetPassword/index.tsx
@@ -4,42 +4,47 @@ import Logo from "../../assets/Quiz_logo.png";
 
 const ResetPassword = () => {
   return (
-    <div className="flex w-full h-screen relative">
+    <div className="flex flex-col md:flex-row w-full h-screen relative">
       <Link to="/" className="absolute top-5 left-5">
         <img src={Logo} alt="Logo" className="w-16 h-auto object-contain" />
       </Link>
 
-      <div className="w-1/3 bg-[#fcfcfc] flex flex-col justify-center items-center p-10">
-        <h2 className="text-3xl font-bold text-gray-800 mb-8">Redefinir Senha</h2>
-        <form className="w-full max-w-[300px]">
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              E-mail
-            </label>
-            <input
-              type="email"
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-[#4a90e2]"
-              placeholder="Digite seu e-mail"
-            />
-          </div>
+      {/* Conteúdo do Formulário */}
+      <div className="flex flex-1 items-center justify-center p-10 bg-[#fcfcfc]">
+        <div className="w-full max-w-[300px]">
+          <h2 className="text-3xl font-bold text-gray-800 mb-8 text-center">
+            Redefinir Senha
+          </h2>
+          <form className="w-full">
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2">
+                E-mail
+              </label>
+              <input
+                type="email"
+                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-[#4a90e2]"
+                placeholder="Digite seu e-mail"
+              />
+            </div>
 
-          <button className="w-full bg-[#529E8D] text-white py-2 rounded-lg hover:bg-[#7b7a7a] transition cursor-pointer">
-            Enviar link de redefinição
-          </button>
+            <button className="w-full bg-[#529E8D] text-white py-2 rounded-lg hover:bg-[#7b7a7a] transition cursor-pointer">
+              Enviar link de redefinição
+            </button>
 
-          <div className="mt-4 text-center">
-            <span className="text-sm text-gray-600">Lembrou sua senha? </span>
-            <Link
-              to="/login"
-              className="text-sm text-gray-600 font-semibold hover:underline"
-            >
-              Faça login
-            </Link>
-          </div>
-        </form>
+            <div className="mt-4 text-center">
+              <span className="text-sm text-gray-600">Lembrou sua senha? </span>
+              <Link
+                to="/login"
+                className="text-sm text-gray-600 font-semibold hover:underline"
+              >
+                Faça login
+              </Link>
+            </div>
+          </form>
+        </div>
       </div>
 
-      <div className="w-2/3 bg-[#FFF7E3] flex items-center justify-center overflow-hidden">
+      <div className="hidden md:flex w-2/3 bg-[#FFF7E3] items-center justify-center overflow-hidden">
         <img
           src={Tela}
           alt="Quiz Illustration"
