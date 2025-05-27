c50d0efdb412a6231d0d6d0a051931f0a68f2b75
Samuel Ricardo <samuelric4rdo@gmail.com>
2025-05-26 22:49:55 -0300

    fix styles

--- src/pages/Perguntas/index.tsx
+++ src/pages/Perguntas/index.tsx
@@ -89,7 +89,7 @@ export default function Perguntas() {
         </div>
         <button
           onClick={restart}
-          className="px-4 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700 transition"
+          className="px-4 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700 transition cursor-pointer"
         >
           Jogar novamente
         </button>
@@ -187,7 +187,7 @@ export default function Perguntas() {
 
           <button
             onClick={nextQuestion}
-            className="mt-2 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
+            className="mt-2 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition cursor-pointer"
           >
             Próxima pergunta
           </button>
